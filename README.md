eb629ccb075dfa00bb85cf92ea605ecdcc24b5db
Laurentiu Ciobanu <laurentiu.ciobanu@sessions.us>
2022-11-24 08:46:07 +0000

    basic webpack config

---
+++ README.md
@@ -0,0 +1,5 @@
+# {name}
+
+## Building and runnning
+
+To build the project run `yarn build`. The outputs of the build will be available in the `build` directory. To start a dev server, watch the source and build on change run `yarn serve`.
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.tsx",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "build"),
+    },
+};
